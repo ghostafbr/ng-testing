c41864eb08736c6bcfc0d1eb73f33819904e455d
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-20 23:02:49 -0500

    Additional settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PicoPreviewComponent } from './components/pico-preview/pico-preview.component';
 import { ProductsComponent } from './components/products/products.component';
+import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { ProductsComponent } from './components/products/products.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ProductsService} from "../../services/products.service";
+import {Product} from "../../models/product.model";
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products: Product[] = [];
+  private productsService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    return this.productsService.getAllSimple().subscribe((products) => {
+      this.products = products;
+    });
+  }
 
 }
