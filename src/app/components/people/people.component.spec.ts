5182fd3175b6e93ad99f98c9557df72e9ff7e0f5
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-23 01:11:14 -0500

    Testing components with dependencies

--- src/app/components/people/people.component.spec.ts
+++ src/app/components/people/people.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PeopleComponent } from './people.component';
+import {PersonComponent} from "../person/person.component";
+import {PersonModel} from "../../models/person.model";
+import {By} from "@angular/platform-browser";
 
 describe('PeopleComponent', () => {
   let component: PeopleComponent;
@@ -8,7 +11,7 @@ describe('PeopleComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PeopleComponent]
+      declarations: [PeopleComponent, PersonComponent]
     });
     fixture = TestBed.createComponent(PeopleComponent);
     component = fixture.componentInstance;
@@ -18,4 +21,40 @@ describe('PeopleComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a list app-person component', () => {
+    // Arrange
+    component.people = [
+      new PersonModel('Juan', 'test',18, 80, 1.80),
+      new PersonModel('Felipe', 'Lopéz',19, 90, 1.60)
+    ];
+    // Act
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.queryAll(By.css('app-person'));
+    // Assert
+    expect(debugElement.length).toEqual(component.people.length);
+  });
+
+  it('should rise an event when clicked', () => {
+    // Arrange
+    const debugElement = fixture.debugElement.query(By.css('app-person .btn-choose'));
+    // Act
+    debugElement.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    // Assert
+    expect(component.selectedPerson).toEqual(component.people[0]);
+  });
+
+  it('should render the selected person', () => {
+    // Arrange
+    const buttonDebug = fixture.debugElement.query(By.css('app-person .btn-choose'));
+    // Act
+    buttonDebug.triggerEventHandler('click', null);
+    fixture.detectChanges();
+    const liDebug = fixture.debugElement.query(By.css('.selectedPerson ul > li'));
+    // Assert
+    expect(component.selectedPerson).toEqual(component.people[0]);
+    expect(liDebug.nativeElement.textContent).toContain(component.selectedPerson?.name);
+  });
+
 });
--- src/app/components/people/people.component.ts
+++ src/app/components/people/people.component.ts
@@ -9,5 +9,14 @@ import {PersonModel} from "../../models/person.model";
 export class PeopleComponent {
 
   person: PersonModel = new PersonModel('Juan', 'test',18, 80, 1.80);
+  people: PersonModel[] = [
+    new PersonModel('Juan', 'test',18, 80, 1.80),
+    new PersonModel('Felipe', 'Lopéz',19, 90, 1.60)
+  ];
+  selectedPerson!: PersonModel | null;
+
+  choosePerson(person: PersonModel) {
+    this.selectedPerson = person;
+  }
 
 }
