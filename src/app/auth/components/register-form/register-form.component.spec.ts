65450a95d331a6afe8d19b1276a2d6aabb3080e0
Andrés Felipe Bolaños <ghostafbr@gmail.com>
2023-10-25 20:27:23 -0500

    Update register-form.component.spec.ts

--- src/app/auth/components/register-form/register-form.component.spec.ts
+++ src/app/auth/components/register-form/register-form.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterFormComponent } from './register-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('RegisterFormComponent', () => {
   let component: RegisterFormComponent;
@@ -10,7 +11,7 @@ describe('RegisterFormComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [RegisterFormComponent],
-      imports: [ ReactiveFormsModule ],
+      imports: [ ReactiveFormsModule, HttpClientTestingModule ],
       providers: []
     });
     fixture = TestBed.createComponent(RegisterFormComponent);
